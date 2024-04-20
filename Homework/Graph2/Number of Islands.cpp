25fdda0ad9a550f37e35cbe58c7eb2098bd879a0
sushilsingh3112 <163157421+sushilsingh3112@users.noreply.github.com>
2024-04-21 01:27:03 +0530

    Create Number of Islands.cpp

---
+++ Homework/Graph2/Number of Islands.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+int m;
+int n;
+void dfs(vector<vector<char>>&grid,int i,int j){
+   if(i<0||i>=m||j<0||j>=n||grid[i][j]!='1'){
+        return ;
+
+    }
+    if(grid[i][j]==-1){
+        return;
+    }
+    grid[i][j]='#';//mark visited
+    dfs(grid,i+1,j);
+    dfs(grid,i-1,j);
+    dfs(grid,i,j+1);
+    dfs(grid,i,j-1);
+}
+
+    int numIslands(vector<vector<char>>& grid) {
+     m=grid.size();
+    n=grid[0].size();
+    int islands=0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+             if(grid[i][j]=='1'){
+                 dfs(grid,i,j);
+                 islands++;
+                    
+                }
+
+            }
+        }
+        return islands;
+    }
+    
+};
