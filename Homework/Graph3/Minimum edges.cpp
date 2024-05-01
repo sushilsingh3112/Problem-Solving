81a6a969228d3af1b5f248d2661c0ac66e2d83b8
sushilsingh3112 <163157421+sushilsingh3112@users.noreply.github.com>
2024-05-01 18:07:21 +0530

    Create Minimum edges.cpp

---
+++ Homework/Graph3/Minimum edges.cpp
@@ -0,0 +1,48 @@
+class Solution{
+public:
+    int minimumEdgeReversal(vector<vector<int>>& edges, int n, int src, int dst) {
+        unordered_map<int, vector<pair<int, int>>> adj;
+        
+        for (auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            adj[u].push_back({v, 0}); 
+            adj[v].push_back({u, 1}); 
+        }
+        
+        vector<int> dist(n + 1, INT_MAX);
+        set<pair<int, int>> s;
+        
+
+        s.insert({0, src});
+        dist[src] = 0;
+        
+        // Dijkstra's algorithm 
+        while (!s.empty()) {
+            auto topElement = s.begin();
+            int topDist = topElement->first;
+            int topNode = topElement->second;
+            s.erase(topElement);
+            
+            for (auto& nbrPair : adj[topNode]) {
+                int nbrNode = nbrPair.first;
+                int nbrDist = nbrPair.second;
+                
+                if (topDist + nbrDist < dist[nbrNode]) {
+                    auto pairSourceEntry = s.find({dist[nbrNode], nbrNode});
+                    if (pairSourceEntry != s.end()) {
+                        s.erase(pairSourceEntry);
+                    }
+                    dist[nbrNode] = topDist + nbrDist;
+                    s.insert({dist[nbrNode], nbrNode});
+                }
+            }
+        }
+        
+        if (dist[dst] != INT_MAX) {
+            return dist[dst];
+        }
+        
+        return -1; // If destination is unreachable
+    }
+};
