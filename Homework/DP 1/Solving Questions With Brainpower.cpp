1ad2bef38bc314bebe024aafb29b49002776b9a1
sushilsingh3112 <163157421+sushilsingh3112@users.noreply.github.com>
2024-05-10 13:08:52 +0530

    Create Solving Questions With Brainpower.cpp

---
+++ Homework/DP 1/Solving Questions With Brainpower.cpp
@@ -0,0 +1,22 @@
+
+class Solution {
+public:
+    long long solve(vector<vector<int>>& questions, int i, vector<long long>& dp) {
+        if (i >= questions.size()) {
+            return 0;
+        }
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+        long long take = questions[i][0] + solve(questions, i + questions[i][1] + 1, dp);
+        long long skip = solve(questions, i + 1, dp);
+        dp[i] = max(take, skip); 
+        return dp[i];       
+    }
+
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        vector<long long> dp(n, -1);
+        return solve(questions, 0, dp);
+    }
+};
